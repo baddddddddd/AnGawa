92d652bc7e7f70d15510a22816ea3639dd674c7d
Alexgillol <apenuliar75@gmail.com>
2023-10-20 22:49:53 +0800

    Update users.py

--- users.py
+++ users.py
@@ -8,6 +8,10 @@ class UserController:
     def authenticate_user(self, username, password):
         query = "SELECT * FROM users WHERE username = %s"
         user = self.db.execute_query(query, (username,))
+        if user and bcrypt.checkpw(password.encode('utf-8'), user[0]['password'].encode('utf-8')):
+            return user[0]
+        else:
+            return None
 
     def register_user(self, username, password):
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
