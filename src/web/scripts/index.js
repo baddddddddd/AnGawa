ff2303676887cc4be62b420e5520e1d7546b90b7
Deogenes Maranan <maranandeogenes@gmail.com>
2023-11-14 00:07:57 +0800

    Added Slideshow of Features

--- src/web/scripts/index.js
+++ src/web/scripts/index.js
@@ -1,51 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var getStartedButton = document.getElementById('getStartedButton');
-  
-    getStartedButton.addEventListener('mouseover', function () {
-      getStartedButton.classList.add('hoverEffect');
-    });
-  
-    getStartedButton.addEventListener('mouseout', function () {
-      getStartedButton.classList.remove('hoverEffect');
-    });
-  
-    getStartedButton.addEventListener('mousedown', function () {
-      getStartedButton.classList.add('clickEffect');
-    });
-  
-    getStartedButton.addEventListener('mouseup', function () {
-      getStartedButton.classList.remove('clickEffect');
-    });
-
-
-  });
-
-document.addEventListener('DOMContentLoaded', function () {
-  var getStartedButtonOutline = document.getElementById('getStartedButtonOutline');
-
-  getStartedButtonOutline.addEventListener('mouseover', function () {
-    getStartedButtonOutline.classList.add('hoverEffect');
-  });
-
-  getStartedButtonOutline.addEventListener('mouseout', function () {
-    getStartedButtonOutline.classList.remove('hoverEffect');
-  });
-
-  getStartedButtonOutline.addEventListener('mousedown', function () {
-    getStartedButtonOutline.classList.add('clickEffect');
-  });
-
-  getStartedButtonOutline.addEventListener('mouseup', function () {
-    getStartedButtonOutline.classList.remove('clickEffect');
-  });
-
-
-});
-
 const imageCarousel = document.getElementById('image-carousel');
 const imageDescriptions = document.querySelectorAll('.image-description');
 
 let currentIndex = 0;
+let slideshowTimeout;
 
 function showImage(index) {
   const images = imageCarousel.querySelectorAll('img');
@@ -61,13 +18,19 @@ function showImage(index) {
 function nextImage() {
   currentIndex = (currentIndex + 1) % imageDescriptions.length;
   showImage(currentIndex);
+  startSlideshowTimer();
 }
 
 function prevImage() {
   currentIndex = (currentIndex - 1 + imageDescriptions.length) % imageDescriptions.length;
   showImage(currentIndex);
+  startSlideshowTimer();
+}
+
+function startSlideshowTimer() {
+  clearTimeout(slideshowTimeout);
+  slideshowTimeout = setTimeout(nextImage, 3000);
 }
 
 showImage(currentIndex);
-
-  
+startSlideshowTimer();
