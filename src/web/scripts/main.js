273a300827f6cfcd34bfcafb294ce187859e6192
Eluxe <eluxeofficial@gmail.com>
2023-11-15 12:32:30 +0800

    Save auth cookies on login and signup

--- src/web/scripts/main.js
+++ src/web/scripts/main.js
@@ -1,11 +1,32 @@
-const body = document.querySelector("body"),
-      sidebar = body.querySelector(".sidebar")
-
-sidebar.addEventListener("mouseenter", () => {
-  sidebar.classList.remove("close");
-})
+import { CookieManager } from "./cookies.js";
 
 
-sidebar.addEventListener("mouseleave", () => {
-  sidebar.classList.add("close");
-})
+async function initializeCookies() {
+    const refreshToken = CookieManager.getCookie("refreshToken");
+
+    if (refreshToken == null) {
+        return;
+    }
+
+    await fetch("http://localhost:5000/api/refresh", {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Authorization": `Bearer ${refreshToken}`,
+            "Content-Type": "application/json",
+        },
+    })
+    .then((response) => response.json())
+    .then((result) => {
+        const accessToken = result["access_token"];
+
+        CookieManager.setCookie("accessToken", accessToken, 0, 1);
+        document.location.replace("./dashboard.html");
+
+    }).catch((err) => console.error(err));
+}
+
+
+CookieManager.eraseCookie("refreshToken");
+
+await initializeCookies();
