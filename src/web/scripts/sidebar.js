30c62cfc3f57d5c29476b6a3ec5057a2e9e06f39
Eluxe <eluxeofficial@gmail.com>
2023-11-15 20:29:40 +0800

    Fix miscopy in sidebar.js

--- src/web/scripts/sidebar.js
+++ src/web/scripts/sidebar.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-//const body = document.querySelector("body"),
-//      sidebar = body.querySelector(".sidebar")
-//
-//sidebar.addEventListener("mouseenter", () => {
-//  sidebar.classList.remove("close");
-//})
-//
-//
-//sidebar.addEventListener("mouseleave", () => {
-//  sidebar.classList.add("close");
-//})
-=======
 const body = document.querySelector("body"),
       sidebar = body.querySelector(".sidebar")
 
@@ -22,8 +9,6 @@ sidebar.addEventListener("mouseenter", () => {
 sidebar.addEventListener("mouseleave", () => {
   sidebar.classList.add("close");
 })
->>>>>>> 66a23d1f3c0b39ff88bb1ebe733e480791f6b228
-
 
 const cards = document.querySelectorAll(".card")
 
