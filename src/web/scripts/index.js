5b1f8215579c974c14efd50bb1ae1dd969653d76
Deogenes Maranan <123554998+DeogenesMaranan@users.noreply.github.com>
2023-11-16 17:56:42 +0800

    Removed Header and Footer

--- src/web/scripts/index.js
+++ src/web/scripts/index.js
@@ -1,28 +1,3 @@
-const toggleButton = document.getElementById('toggleButton');
-const dropdown = document.querySelector('.dropdown'); // Changed to select by class
-
-toggleButton.addEventListener('click', () => {
-  toggleButton.innerHTML = toggleButton.innerHTML === '<i class="fas fa-bars"></i>' ? '<i class="fas fa-times"></i>' : '<i class="fas fa-bars"></i>';
-  updateDropdownDisplay();
-});
-
-window.addEventListener('resize', () => {
-  if (window.innerWidth >= 600) {
-    hideDropdown();
-  }
-});
-
-function updateDropdownDisplay() {
-  if (window.innerWidth < 600) {
-    dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
-  }
-}
-
-function hideDropdown() {
-  dropdown.style.display = 'none';
-  toggleButton.innerHTML = '<i class="fas fa-bars"></i>';
-}
-
 const imageCarousel = document.getElementById('image-carousel');
 const imageDescriptions = document.querySelectorAll('.image-description');
 
