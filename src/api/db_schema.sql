7a21fbf27cc940c771fcec87840719a9b808793e
Alexgillol <apenuliar75@gmail.com>
2023-12-06 00:20:32 +0800

    Update db_schema.sql

--- src/api/db_schema.sql
+++ src/api/db_schema.sql
@@ -1,17 +1,43 @@
 
-CREATE TABLE users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+CREATE TABLE Users (
+    UserId INT AUTO_INCREMENT PRIMARY KEY,
+    Pfp JSON,
+    FirstName VARCHAR(255),
+    MiddleName VARCHAR(255),
+    LastName VARCHAR(255),
+    NameExt VARCHAR(255),
+    BIRTHDATE DATE,
+    GENDERT ENUM('Male', 'Female', 'Other')
+    Email VARCHAR(255) NOT NULL,
+    HashedPw VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE tasks (
-    task_id INT AUTO_INCREMENT PRIMARY KEY,
-    description TEXT,
-    deadline DATETIME,
-    duration INT,
-    priority INT
+CREATE TABLE Tasks (
+    TaskId INT,
+    TaskName VARCHAR(255),
+    Description TEXT,
+    Deadline DATETIME,
+    Duration INT,
+    Priority INT,
+    FatiguingLevel INT,
+    UserId INT,
+    Status ENUM('Pending','Completed')
+    PRIMARY KEY(TaskId, UserId)
 );
 
+CREATE TABLE Notes(
+    NoteId INT AUTO_INCREMENT PRIMARY KEY,
+    UserId INT,
+    FOREIGN KEY(UserId) REFERENCES Users(UserId),
+    NoteTitle VARCHAR(255) NOT NULL,
+    NoteContent JSON,
+    LastModified DATETIME
+);
+
+CREATE TABLE UserSettings (
+    UserId INT,
+    TotalEnergy INT,
+    WorkTime JSON,
+    PRIMARY KEY (UserId)
+);
 
